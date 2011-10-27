52f24f4964bb6df0a437b2a0e7b773e534dcdf95
future <commanda.keen@gmail.com>
2011-10-27 18:24:06 +0200

    The Thread is actually doing something!

--- SongImporter.h
+++ SongImporter.h
@@ -1,14 +1,19 @@
-#import <Cocoa/Cocoa.h>
+@class SongsArrayController;
 
 @interface SongImporter : NSObject {
 
   NSString *filepath;
   NSXMLDocument *document;
   
+  // Array Controllers
+  SongsArrayController *songsArrayController;
+
 }
 
 @property (nonatomic, retain, readwrite) NSString *filepath;
 @property (nonatomic, retain, readwrite) NSXMLDocument *document;
+
+@property (nonatomic, retain, readonly) SongsArrayController *songsArrayController;
 
 - (void) import;
 
--- SongImporter.m
+++ SongImporter.m
@@ -4,6 +4,7 @@
 #import "ApplicationDelegate.h"
 #import "MainWindowController.h"
 #import "ProgressWindowController.h"
+#import "SongsArrayController.h"
 
 @implementation SongImporter
 
@@ -21,8 +22,33 @@
   self.document = [[NSXMLDocument alloc] initWithData:data options:0 error:&error];
   NSString *rootName = [[self.document rootElement] name];
   if ([rootName isEqualToString:@"EasiSlides"]) {
-    [self importEasislides];
+    debugLog(@"forking");
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(importEasislidesStarter:) object:nil];
+    
+    
+    debugLog(@"Starting...");
+    [thread start];
+    
+    while ([thread isExecuting]) {
+      debugLog(@"Executing...");
+      sleep(1);
+    }
+    
+    debugLog(@"Finished...");
+    
+    //[self importEasislides];
+    
+    
   }
+}
+
+- (void) importEasislidesStarter:sender {
+  debugLog(@"importEasislidesStarter called");
+  // This AutoreleasePool is mandatory for every Thread. Basically, it prevents
+  // the code between here and "[pool drain]" to blow your memory into pieces.
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  [self importEasislides];
+  [pool drain];
 }
 
 - (void) importEasislides {
@@ -33,7 +59,7 @@
   for (NSXMLElement* element in [[self.document rootElement] children]) {
     float progress = ((float)i / songsCount) * 100;
     
-    //debugLog(@"element: %@", [[element children] objectAtIndex:0]);
+    debugLog(@"element: %@", [[element children] objectAtIndex:0]);
     
     NSString *title    = [[[element elementsForName:@"Title1"] objectAtIndex:0] stringValue];
     NSString *content  = [[[element elementsForName:@"Contents"] objectAtIndex:0] stringValue];
@@ -47,23 +73,50 @@
     if (![license1 isEqualToString:@""]) footnote = [footnote stringByAppendingFormat:@", %@", license1];
     if (![license2 isEqualToString:@""]) footnote = [footnote stringByAppendingFormat:@", %@", license2];
     
+    debugLog(@"addsong start...");
+
     [self addSong:title withContent:content andFootnote:footnote];
+
+    debugLog(@"addsong stop...");
+
+    //[[self progressWindowController] setProgressValue:progress];
     
-    [[self progressWindowController] setProgressValue:progress];
     
     i++;
-    //if (i >= 1) break;
+    if (i >= 100) break;
   }
-  
+  debugLog(@"finished.");
+
   [[self progressWindowController] orderOut];
 }
 
 - (void) addSong:(NSString*)title withContent:(NSString*)content andFootnote:(NSString*)footnote {
+  debugLog(@"adding song!");
   Song *song = [[NSApp songsArrayController] newObject];
   [song setValue:title forKey:@"title"];
   [song setValue:content forKey:@"content"];
   [song setValue:footnote forKey:@"footnote"];
-  [[NSApp songsArrayController] addObject:song];
+  debugLog(@"before array %@", NSApp);
+  debugLog(@"before array %@", [NSApp songsArrayController]);
+  [[self songsArrayController] addObject:song];
+  debugLog(@"after array");
+}
+
+- (id) managedObjectContextForThread; {    
+  NSManagedObjectContext * newContext = [[[NSThread currentThread] threadDictionary] valueForKey:@"managedObjectContext"];
+  if(newContext) return newContext;
+  
+  newContext = [NSManagedObjectContext new];
+  [newContext setPersistentStoreCoordinator:[NSApp persistentStoreCoordinator]];
+  [[[NSThread currentThread] threadDictionary] setValue:newContext forKey:@"managedObjectContext"];
+  return newContext;
+}
+
+- (SongsArrayController*) songsArrayController {
+ if (songsArrayController) return songsArrayController;
+ songsArrayController = [SongsArrayController new];
+ [songsArrayController setManagedObjectContext:[self managedObjectContextForThread]];
+ return songsArrayController;
 }
 
 /********************
